cf4c90495564bae2d7914eef7d0d497494f968ea
hasnagagah <nengkya@yahoo.com>
2021-03-23 09:13:26 +0700

    Bisa git belakangan, ikutin suggest terminal ubuntu

--- app/Http/Controllers/PegawaiController.php
+++ app/Http/Controllers/PegawaiController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support]\Facades\DB
+use Illuminate\Support\Facades\DB
 
 class PegawaiController extends Controller
 {
 	public function index(){
 		return view('index');
-
-
 	}
 }
